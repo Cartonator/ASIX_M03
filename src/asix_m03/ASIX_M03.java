376efa9cc56775b5dc8741d7e7f884d4f997fb34
Eric <Eric@ERIC>
2014-09-26 16:36:29 +0200

    Commit inicial

---
+++ src/asix_m03/ASIX_M03.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asix_m03;
+
+/**
+ *
+ * @author Eric
+ */
+public class ASIX_M03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 4;
+        int n2 = 5;
+        int resultado;
+        resultado = n1 + n2;
+                
+        System.out.println("resultado " + resultado);
+        
+        
+    }
+    
+}
